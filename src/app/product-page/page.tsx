e0d6721b6d7fabce1863bc8a0d6e7434f7680176
Jean <jean.mauricio@industriascin.com>
2025-05-09 17:19:20 -0400

    added filter by name

--- src/app/product-page/page.tsx
+++ src/app/product-page/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Flex, Spin } from "antd";
+import { Button, Flex, Input, Space, Spin } from "antd";
 import Link from "next/link";
 import ProductTable from "../components/productTable/productTable";
 import CategoryTable from "../components/categoryTable/categoryTable";
@@ -8,11 +8,25 @@ import {
   useCategoryQuery,
   useProductQuery,
 } from "@/features/product/productQuery/query";
+import { useMemo, useState } from "react";
+import { debounce } from "lodash";
 
 export default function PageProduct() {
   const { isPending: isProductPending, data: productData } = useProductQuery();
   const { isPending: isCategoryPending, data: categoryData } =
     useCategoryQuery();
+  const [name, setName] = useState<string>("");
+
+  const debounceSearch = useMemo(
+    () =>
+      debounce((value: string) => {
+        setName(value);
+      }, 500),
+    []
+  );
+  const filteredProducts = productData?.products?.filter((product) =>
+    product.name.toLowerCase().includes(name.toLowerCase())
+  );
   return (
     <>
       <Flex justify="space-between" align="center" style={{ margin: 8 }}>
@@ -23,8 +37,25 @@ export default function PageProduct() {
         </div>
       </Flex>
 
+      <Space wrap>
+        <Input
+          placeholder="Buscar por nombre"
+          id="name"
+          autoComplete="off"
+          onChange={(e) => {
+            debounceSearch(e.target.value);
+            setName(e.target.value);
+          }}
+          allowClear
+          value={name}
+          count={{
+            max: 255,
+            exceedFormatter: (txt, { max }) => txt.slice(0, max),
+          }}
+        />
+      </Space>
       <Spin spinning={isProductPending} tip="Cargando productos...">
-        <ProductTable data={productData?.products || []} />
+        <ProductTable data={filteredProducts || []} />
       </Spin>
       <div style={{ marginTop: 30, textAlign: "center" }}>
         <h2>Lista de Totales Por Categorias</h2>
